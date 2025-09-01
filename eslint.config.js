bf3a11ced059f31ec2e4fc88e92d09c8d370b627
Deshen Rao <deshen.r1502@gmail.com>
2025-09-01 22:24:17 +0200

    chore: set up typescript, eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/coverage/**', 'data/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      eqeqeq: ['error', 'always'],
+    },
+  },
+  prettier,
+);
